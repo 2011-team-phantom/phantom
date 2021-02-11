d91ad7e112c751b66eff86a50a9e9165c75529c6
kaitbr0 <humansare@gmail.com>
2021-02-11 16:45:51 -0500

    move error handler

--- server/api/index.js
+++ server/api/index.js
@@ -1,33 +1,36 @@
-const router = require('express').Router();
-const User = require('../db/models/users');
-const db = require('../db');
-const dotenv = require('dotenv').config();
-const plaid = require('plaid');
+const router = require("express").Router();
+const User = require("../db/models/users");
+const db = require("../db");
+const dotenv = require("dotenv").config();
+const plaid = require("plaid");
 
+function handleError(errorMessage) {
+  console.error(errorMessage);
+}
 const client = new plaid.Client({
   clientID: process.env.PLAID_CLIENT_ID,
   secret: process.env.PLAID_SECRET,
   env: plaid.environments.sandbox,
 });
 
-router.get('/', function (req, res, next) {
-  console.log('connected');
+router.get("/", function (req, res, next) {
+  console.log("connected");
 });
 
-router.get('/linkTokenCreate', async (req, res) => {
+router.get("/linkTokenCreate", async (req, res) => {
   try {
     const { link_token } = await client.createLinkToken({
       user: {
-        client_user_id: '123-test-user-id',
+        client_user_id: "123-test-user-id",
       },
-      client_name: 'Plaid Test App',
-      products: ['auth', 'transactions'],
-      country_codes: ['US'],
-      language: 'en',
+      client_name: "Plaid Test App",
+      products: ["auth", "transactions"],
+      country_codes: ["US"],
+      language: "en",
       // webhook: 'https://sample-web-hook.com',
       account_filters: {
         depository: {
-          account_subtypes: ['checking', 'savings'],
+          account_subtypes: ["checking", "savings"],
         },
       },
     });
@@ -37,9 +40,9 @@ router.get('/linkTokenCreate', async (req, res) => {
   }
 });
 
-router.post('/plaidTokenExchange', async (req, res) => {
+router.post("/plaidTokenExchange", async (req, res) => {
   try {
-    const user = await db.collection('users').findOne({ _id: req.user._id });
+    const user = await db.collection("users").findOne({ _id: req.user._id });
     const { publicToken } = req.body;
 
     if (user.access_token.length) {
@@ -55,7 +58,7 @@ router.post('/plaidTokenExchange', async (req, res) => {
         .getAccounts(access_token)
         .catch(handleError);
 
-      db.collection('users').updateOne(
+      db.collection("users").updateOne(
         { _id: req.user._id },
         { $set: { access_token } }
       );
@@ -67,15 +70,15 @@ router.post('/plaidTokenExchange', async (req, res) => {
   }
 });
 
-router.get('/plaidTransactions', async (req, res) => {
+router.get("/plaidTransactions", async (req, res) => {
   try {
     const today = new Date();
-    const dd = String(today.getDate()).padStart(2, '0');
-    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const dd = String(today.getDate()).padStart(2, "0");
+    const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
     const yyyy = today.getFullYear();
 
-    const now = yyyy + '-' + mm + '-' + dd;
-    const lastYear = yyyy - 1 + '-' + mm + '-' + dd;
+    const now = yyyy + "-" + mm + "-" + dd;
+    const lastYear = yyyy - 1 + "-" + mm + "-" + dd;
 
     const data = await client.getTransactions(
       req.user.access_token,
@@ -92,7 +95,7 @@ router.get('/plaidTransactions', async (req, res) => {
   }
 });
 
-router.post('/budget', async function (req, res, next) {
+router.post("/budget", async function (req, res, next) {
   try {
     const budgetUser = await User.updateOne(
       { _id: req.user._id },
@@ -104,20 +107,20 @@ router.post('/budget', async function (req, res, next) {
     );
     res.send(updatedUser.budget);
   } catch (error) {
-    console.log('Error adding budget:', error);
+    console.log("Error adding budget:", error);
   }
 });
 
-router.get('/budget', async function (req, res, next) {
+router.get("/budget", async function (req, res, next) {
   try {
     const user = await User.findOne({ _id: req.user._id });
     res.send(user);
   } catch (error) {
-    console.log('Error getting budget:', error);
+    console.log("Error getting budget:", error);
   }
 });
 
-router.put('/budget', async function (req, res, next) {
+router.put("/budget", async function (req, res, next) {
   try {
     const user = await User.findOne({ _id: req.user._id }, { budget: 1 });
     const budgetUser = await User.updateOne(
@@ -130,12 +133,12 @@ router.put('/budget', async function (req, res, next) {
     );
     res.send(updatedUser.budget);
   } catch (error) {
-    console.log('Error updating budget:', error);
+    console.log("Error updating budget:", error);
   }
 });
 
 router.use(function (req, res, next) {
-  const err = new Error('Not found.');
+  const err = new Error("Not found.");
   err.status = 404;
   next(err);
 });
